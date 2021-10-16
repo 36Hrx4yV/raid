3995fbe57062c699ad141601d69314e095559c50
36Hrx4yV <88466366+36Hrx4yV@users.noreply.github.com>
2021-10-16 19:39:17 +0800

    Create sus.py

--- sus.py
+++ sus.py
@@ -1,5 +1,6 @@
 from zipfile import ZipFile 
-file_name = "lol.zip"
+print("Enter file to unzip")
+file_name = input() 
 with ZipFile(file_name, 'r') as zip: 
     zip.printdir() 
     print('Extracting all the files now...') 
